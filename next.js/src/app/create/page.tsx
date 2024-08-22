d6db335a8c3717e58f30dc5fb0b1c16d944fc238
madforre <madforre@madforreui-MacBookPro.local>
2024-08-22 19:39:19 +0900

    feat: 글 생성

--- next.js/src/app/create/page.tsx
+++ next.js/src/app/create/page.tsx
@@ -1,10 +1,61 @@
+"use client";
+
+import { useRouter } from "next/navigation"; // next/router는 페이지 라우터 방식으로 사용되는 next 12 버젼 라우터임. 이상 버젼에서는 네비게이션에서 가져와야함
+import { FormEvent, useState } from "react";
 
 // 현재 디렉토리에 layout 파일이 없으면 부모 디렉토리에 가서 layout 파일을 찾음.
 
 // create 폴더 내에 layout.js 또는 layout.jsx가 있으면 ?
-
 export default function Create() {
+
+  const [count, setCount] = useState("0");
+
+  fetch(`http://localhost:9999/topics`)
+    .then(res => res.json())
+    .then(result => {
+      setCount(result.length);
+  });
+
+  const router = useRouter();
+
   return (
-      <>Create!!</>
+    // onSubmit은 사용자와 상호작용할 때 실행된다! 이것은 서버컴포넌트에서 다루지 않고 클라이언트 컴포넌트에서 다룸!!
+    <form onSubmit={(e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault(); // onSubmit 실행시 기본적인 동작을 방지시킨다. (예를 들면 해당 네임 관련된 파라미터가 적힌 페이지 이동)
+        const target = e.target;
+        let { title, body }: any = target;
+        title = title.value;
+        body = body.value;
+
+        const new_cnt = (parseInt(count)+1).toString();
+
+        const options = {
+          method: 'POST',
+          header: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({title, body, id: new_cnt})
+        }
+
+        fetch(`http://localhost:9999/topics`, options)
+          .then(res => res.json())
+          .then(result => {
+            console.log(result);
+            const last_id = result.id;
+            router.push(`/read/${last_id}`);
+          })
+
+    }}>
+      현재 글 갯수 : {count};
+      <p>
+        <input type="text" name="title" placeholder="title" />
+      </p>
+      <p>
+        <textarea name="body" placeholder="body"></textarea>
+      </p>
+      <p>
+        <input type="submit" value="create" />
+      </p>
+    </form>
   )
 };
--- next.js/src/app/read/[id]/page.tsx
+++ next.js/src/app/read/[id]/page.tsx
@@ -1,12 +1,15 @@
-export default function Read(
+export default async function Read( // 데이터를 읽어서 출력한 뿐이다? => 서버 컴포넌트로 사용도록 하자.
   props: Readonly<{ children: React.ReactNode, params: {id: string} }> 
 ) {
-    console.log(props);
+  const response = await fetch(`http://localhost:9999/topics/${props.params.id}`);
+  const topics = await response.json();
+    // console.log(props);
     return (
       <>
-      	<h2>Read</h2>
+      	<h2>{topics.title}</h2>
         {/* // 폴더 명에 해당되는 위치의 id 값을 가져올 수 있다. */}
       	parameters : {props.params.id}
+        {topics.body}
       </>
     )
 }
